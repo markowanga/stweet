6dc23bf1a9b655514bc17407faa10b8adfa6b999
Marcin Wątroba <markowanga@gmail.com>
2020-12-18 18:24:10 +0100

    Raise version to 1.1.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r") as fh:
 
 setuptools.setup(
     name="stweet",
-    version="1.0.0",
+    version="1.1.0",
     author="Marcin Wątroba",
     author_email="markowanga@gmail.com",
     description="Package to scrap tweets",

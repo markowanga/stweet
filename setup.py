211af01266679784588e00ad5cbab37312a3455a
Marcin Wątroba <markowanga@gmail.com>
2021-02-15 22:13:24 +0100

    Release version 1.3.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r") as fh:
 
 setuptools.setup(
     name="stweet",
-    version="1.2.2",
+    version="1.3.0",
     author="Marcin Wątroba",
     author_email="markowanga@gmail.com",
     description="Package to scrap tweets",
